05520633a856911ceb1a15b0151ec1ee23bba746
NasarTarique <nasartarique@gmail.com>
2021-03-13 19:44:58 +0530

    urls

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('join/', views.index),
+    path('video/', views.index),
     path('create/', views.index),
     path('room/', views.index)
 ]
